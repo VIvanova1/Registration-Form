417859c51c0917ece103bfcbe6967b174fdd34b0
Veneta Ivanova <i.veneta.v@gmail.com>
2022-08-27 17:11:28 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+function matchPassword() {
+    const password = document.getElementById('pass');
+    const passwordChek = document.getElementById('confirmPass');
+
+    if (password !== passwordChek) {
+        alert('Passwords did not match')
+    } else {
+
+    }
+}
