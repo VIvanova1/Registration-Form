b0eb95e421de6facc844bd3ddd1e411f650897ac
Veneta Ivanova <i.veneta.v@gmail.com>
2022-10-24 21:56:37 +0300

    simple register button functionality

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-function matchPassword() {
+window.addEventListener("load", solve);
+
+function solve() {
     const password = document.getElementById('pass');
     const passwordChek = document.getElementById('confirmPass');
+    const username = document.getElementById('user-name');
 
-    if (password !== passwordChek) {
-        alert('Passwords did not match')
-    } else {
+    const registerBtnElement = document.getElementById('register-btn');
+    registerBtnElement.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (password.value !== passwordChek.value) {
+            alert('Passwords did not match')
+        }
+        e.currentTarget.parentElement.remove();
 
-    }
+        const mainSection = document.getElementById('main');
+        const h1Element = document.createElement('h1');
+        h1Element.classList.add('title');
+        h1Element.textContent = `Wellcome, ${username.value}!`;
+        mainSection.appendChild(h1Element);
+    });
 }
